cbbe4f5b8686cf31a4b48b475ac17377054778f4
Chris Papageorgakis <Christos.Papageorgakis@cern.ch>
2022-11-18 02:08:56 +0100

    Apply shellcheck suggestions

--- logger.sh
+++ logger.sh
@@ -1,51 +1,51 @@
 #!/bin/sh
 
-touch $SCRIPT_LOG
+# Ignore warnings that are not important for bash
+# shellcheck disable=SC3000-SC4000
 
-function SCRIPTENTRY(){
+touch "$SCRIPT_LOG"
+
+SCRIPTENTRY(){
   timeAndDate=$(date)
   script_name=$(basename "$0")
   script_name="${script_name%.*}"
-  echo "[$timeAndDate] [DEBUG]  > $script_name $FUNCNAME" >> $SCRIPT_LOG
+  echo "[$timeAndDate] [DEBUG]  > $script_name ${FUNCNAME[0]}" >> "$SCRIPT_LOG"
 }
 
-function SCRIPTEXIT(){
+SCRIPTEXIT(){
   script_name=$(basename "$0")
-  ript_name="${script_name%.*}"
-  echo "[$timeAndDate] [DEBUG]  < $script_name $FUNCNAME" >> $SCRIPT_LOG
+  script_name="${script_name%.*}"
+  echo "[$timeAndDate] [DEBUG]  < $script_name ${FUNCNAME[0]}" >> "$SCRIPT_LOG"
 }
 
-function ENTRY(){
+ENTRY(){
   local cfn="${FUNCNAME[1]}"
   timeAndDate=$(date)
-  echo "[$timeAndDate] [DEBUG]  > $cfn $FUNCNAME" >> $SCRIPT_LOG
+  echo "[$timeAndDate] [DEBUG]  > $cfn ${FUNCNAME[0]}" >> "$SCRIPT_LOG"
 }
 
-function EXIT(){
+EXIT(){
   local cfn="${FUNCNAME[1]}"
   timeAndDate=$(date)
-  echo "[$timeAndDate] [DEBUG]  < $cfn $FUNCNAME" >> $SCRIPT_LOG
+  echo "[$timeAndDate] [DEBUG]  < $cfn ${FUNCNAME[0]}" >> "$SCRIPT_LOG"
 }
 
 
-function INFO(){
-  local function_name="${FUNCNAME[1]}"
+INFO(){
   local msg="$1"
   timeAndDate=$(date)
-  echo "[$timeAndDate] [INFO]  $msg" >> $SCRIPT_LOG
+  echo "[$timeAndDate] [INFO]  $msg" >> "$SCRIPT_LOG"
 }
 
 
-function DEBUG(){
-  local function_name="${FUNCNAME[1]}"
+DEBUG(){
   local msg="$1"
   timeAndDate=$(date)
-  echo "[$timeAndDate] [DEBUG]  $msg" >> $SCRIPT_LOG
+  echo "[$timeAndDate] [DEBUG]  $msg" >> "$SCRIPT_LOG"
 }
 
-function ERROR(){
-  local function_name="${FUNCNAME[1]}"
+ERROR(){
   local msg="$1"
   timeAndDate=$(date)
-  echo "[$timeAndDate] [ERROR]  $msg" >> $SCRIPT_LOG
+  echo "[$timeAndDate] [ERROR]  $msg" >> "$SCRIPT_LOG"
 }
